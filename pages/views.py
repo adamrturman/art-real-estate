a7aa6b626afc9c38544be9b39328ba1da5299139
adamrturman <adamrturman@gmail.com>
2020-11-17 12:19:58 -0600

    price, state, and bedroom choices changed from static markup to reference the previously created dictionaries

--- pages/views.py
+++ pages/views.py
@@ -10,7 +10,10 @@ def index(request):
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
 
     context = {
-        'listings': listings
+        'listings': listings,
+        'price_choices': price_choices,
+        'bedroom_choices': bedroom_choices,
+        'state_choices': state_choices
     }
     
     return render(request, 'pages/index.html', context)
