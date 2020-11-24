fc25aa3ac27add5d4e57a4eb218c7738ade1fe77
adamrturman <adamrturman@gmail.com>
2020-11-24 12:10:46 -0600

    removed email from settings

--- adam/settings.py
+++ adam/settings.py
@@ -144,12 +144,7 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
 
-# Email config
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'adamrturmantest'
-EMAIL_HOST_PASSWORD = '1845Code!'
-EMAIL_USE_TLS=True
+
 
 try:
     from .local_settings import *
