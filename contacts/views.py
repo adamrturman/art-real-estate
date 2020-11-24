aabccf183522fa1de0db51e09da31abd3224c9e5
adamrturman <adamrturman@gmail.com>
2020-11-24 09:55:18 -0600

    tested that inquiry form submission does send email to my test account & removed password

--- contacts/views.py
+++ contacts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.core.mail import send_mail
 from .models import Contact
 
 def contact(request):
@@ -21,9 +22,19 @@ def contact(request):
         messages.error(request, 'You have already made an inquiry for this listing')
         return redirect('/listings/'+listing_id)
 
+
     contact = Contact(listing=listing, listing_id=listing_id, name=name, email=email, phone=phone, message=message, user_id=user_id )
 
     contact.save()
+
+    # Send email
+    send_mail(
+        'Property Listing Inquiry',
+        'There has been an inquiry for ' + listing + '. Sign into the admin panel for more information.',
+        'adamrturmantest@gmail.com',
+        [realtor_email, 'adamrturmantest@gmail.com'],
+        fail_silently=False
+    )
 
     messages.success(request, 'Your request has been submitted, a realtor will get back to you soon')
     return redirect('/listings/'+listing_id)
